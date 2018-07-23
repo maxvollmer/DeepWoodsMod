f883e0d60716ed4f4469958dc60f63f8e1a34f28
maxvollmer <maxvollmer@users.noreply.github.com>
2018-07-23 20:51:31 +0200

    Added DeepWoodsSettings for later managing of all config values etc

---
+++ DeepWoodsMod/DeepWoodsSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeepWoodsMod
+{
+    class DeepWoodsSettings
+    {
+        public static HashSet<long> playersWhoGotStardropFromUnicorn = new HashSet<long>();
+    }
+}
